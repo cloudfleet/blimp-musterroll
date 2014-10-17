e7a51b007396b2a9b7e3d178ebd7c99358c0c27e
DoubleMalt <christoph@web.crofting.com>
2014-10-17 15:38:15 +0200

    fix environment variable names

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-authbind node cockpit.js --domain=${BLIMP_DOMAIN} \
-    --secret=${BLIMP_SECRET} > /opt/cloudfleet/log/musterroll.log 2>&1
+authbind node cockpit.js --domain=${CLOUDFLEET_DOMAIN} \
+    --secret=${CLOUDFLEET_SECRET} > /opt/cloudfleet/log/musterroll.log 2>&1
